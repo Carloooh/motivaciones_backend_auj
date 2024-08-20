9cf245729deb4ee070fa9d4ca73e150b77cf0b1a
panchodiaz <pancho.diaz.varas@gmail.com>
2024-08-20 01:20:00 -0400

    Se agrega solución al issue solicitado

--- src/controllers/motivaciones.js
+++ src/controllers/motivaciones.js
@@ -29,12 +29,30 @@ async function addMotivacion(req, res) {
       return res.status(400).json({ error: "Título y descripción son requeridos" });
     }
 
+    etiquetas = etiquetas ? etiquetas.map(tag => tag.trim()) : [];
+
+    if (titulo.length < 5 || titulo.length > 100) {
+      return res.status(400).json({ error: "El título debe tener entre 5 y 100 caracteres" });
+    }
+
+    if (descripcion.length < 20) {
+      return res.status(400).json({ error: "La descripción debe tener al menos 20 caracteres" });
+    }
+
+    if (etiquetas.length > 5) {
+      return res.status(400).json({ error: "No se pueden tener más de 5 etiquetas" });
+    }
+
     const nuevaMotivacion = new Motivacion({ titulo, descripcion, etiquetas });
     await nuevaMotivacion.save();
 
     res.status(201).json(nuevaMotivacion);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    if (error.code === 11000) {
+      res.status(400).json({ error: "El título ya existe, elija otro título" });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
   }
 }
 
@@ -45,6 +63,24 @@ async function updateMotivacion(req, res) {
 
     if (Object.keys(updateData).length === 0) {
       return res.status(400).json({ error: "No hay datos para actualizar" });
+    }
+
+    if (updateData.titulo) {
+      if (updateData.titulo.length < 5 || updateData.titulo.length > 100) {
+        return res.status(400).json({ error: "El título debe tener entre 5 y 100 caracteres" });
+      }
+    }
+
+    if (updateData.descripcion) {
+      if (updateData.descripcion.length < 20) {
+        return res.status(400).json({ error: "La descripción debe tener al menos 20 caracteres" });
+      }
+    }
+
+    if (updateData.etiquetas) {
+      if (updateData.etiquetas. length > 5) {
+        return res.status(400).json({ error: "No se puden tener mas de 5 etiquetas" });
+      }
     }
 
     const motivacion = await Motivacion.findByIdAndUpdate(
@@ -59,7 +95,11 @@ async function updateMotivacion(req, res) {
 
     res.json(motivacion);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    if (error.code === 11000) {
+      return res.status(400).json({ error: "El título ya existe, elija otro título" });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
   }
 }
 
