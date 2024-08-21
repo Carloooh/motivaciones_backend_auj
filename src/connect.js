aee115446ac809ac255ec77dcc6530ee755a0725
Carlos Ignacio Azócar Huerta <carlos.iah17@gmail.com>
2024-08-20 21:14:03 -0400

    fix: eliminación de uso de credenciales directamente en el código para utilizar .env
    Ahora se utiliza un .env para acceder a las credenciales de la base de datos en vez de utilizarlas directamente en el código para aumentar la seguridad

--- src/connect.js
+++ src/connect.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
+require('dotenv').config({ path: '.env' })
 
 async function connectToMongoDB() {
-  // Estoy utilizando una credencial temporal directamente en el código en lugar de ponerla en un archivo .env 
-  // para que los contribuyentes puedan probar la API sin tener que pedirme el enlace y credenciales.
-  // una vez realizadas las contribuciones, cambiaré el código para usar el .env
-  const uri = "mongodb+srv://auj:Ku9Bhfr2bpKuUoqS@auj-postulacion-backend.ezusd.mongodb.net/?retryWrites=true&w=majority&appName=auj-postulacion-backend";
+  const uri = process.env.MONGODB_URI;
   return mongoose.connect(uri);
 }
 
